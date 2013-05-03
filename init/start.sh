f1520628f8fc96a5b311a41a6081896f42e82558
stephen soltesz <stephen.soltesz@gmail.com>
2013-05-03 17:49:54 +0000

    update patter for starting the select servers to nuq01

--- init/start.sh
+++ init/start.sh
@@ -5,7 +5,7 @@ source /etc/mlab/slicectrl-functions
 cd $SLICEHOME/shaperprobe/
 perl $SLICEHOME/shaperprobe/daemon.pl &> /dev/null &
 
-if [[ $(hostname) =~ "nuq0t" ]]; then
+if [[ $(hostname) =~ "nuq01" ]]; then
     cd $SLICEHOME
     $SLICEHOME/init/selector.sh &> /dev/null &
 fi
